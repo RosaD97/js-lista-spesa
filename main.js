179b692b8a4191c4f9fd2af9c5aecf2e7b35ffe5
RosaD97 <r960595@gmail.com>
2023-02-28 15:06:21 +0100

    esercizio completato

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
  'use strict';
 
+ const shoppingList = ['pane', 'vino', 'farina', 'acqua', 'latte'];
+ const lista = document.querySelector('.lista');
+
+ let i = 0;
+ while (i < shoppingList.length){
+    const listItems = document.createElement('li');
+    listItems.append(shoppingList[i]);
+    lista.append(listItems);
+    i++;
+ }
